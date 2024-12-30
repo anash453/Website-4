151f0200aa881ad3af2c7753565daddef1ff8bba
anash453 <anash6291@gmail.com>
2024-12-30 18:06:26 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+
+    // Toggle icon between menu and close icons
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('nav');
+    if (window.scrollY > 500) { // Adjust the scroll threshold as needed
+        navbar.classList.add('fixed');
+    } else {
+            navbar.classList.remove('fixed');
+    }
+    });
+
